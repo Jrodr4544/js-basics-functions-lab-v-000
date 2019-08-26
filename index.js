98b2a55f28d2a0a9dfd26ab76b2190736403ec81
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2019-08-26 17:38:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,9 +18,11 @@ function distanceTravelledInFeet(start, end) {
 function calculatesFarePrice(start, destination) {
   const price = 0;
   const distanceTravelled = distanceTravelledInFeet(start, destination)
-  if (distanceTravelledInFeet(start, destination) < 400) {
+  if (distanceTravelled < 400) {
     return price;
-  } else if (distanceTravelledInFeet(start, destination))
+  } else if (distanceTravelled > 400 && distanceTravelled < 2000) {
+    return price += distanceTravelled * .2
+  }
 }
 
 
