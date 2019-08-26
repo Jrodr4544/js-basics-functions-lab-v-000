b38d1cc8eead942afdab3f3938b088e7a1c48d98
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2019-08-26 17:29:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function distanceFromHqInFeet(location) {
   return distanceFromHqInBlocks(location) * distancePerBlock
 }
 
-function distanceTravelledInFeet(location) {
+function distanceTravelledInFeet(start, end) {
   
 }
